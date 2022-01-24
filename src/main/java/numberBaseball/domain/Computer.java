b1f25c92eb5fc57f52db4386c86b845dab3be390
seheonCho <whtpgjs94@gmail.com>
2022-01-24 22:13:05 +0900

    feat: 임의의 숫자 생성
    상대방(Computer)이 숫자 생성기에서 숫자 리스트를 만들어, 자신에게 할당

--- src/main/java/numberBaseball/domain/Computer.java
+++ src/main/java/numberBaseball/domain/Computer.java
@@ -1,4 +1,17 @@
 package numberBaseball.domain;
 
+import java.util.List;
+
 public class Computer {
+
+    NumberCreater numberCreater;
+    List<Integer> numbers;
+
+    Computer() {
+        numberCreater = new NumberCreater();
+    }
+
+    public void setNumbers() {
+        this.numbers = numberCreater.getNumbers();
+    }
 }
--- src/main/java/numberBaseball/domain/NumberCreater.java
+++ src/main/java/numberBaseball/domain/NumberCreater.java
@@ -1,4 +1,39 @@
 package numberBaseball.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class NumberCreater {
+
+    List<Integer> numbers;
+
+    Random random = new Random();
+
+    NumberCreater() {
+        clearNumbers();
+        this.numbers = new ArrayList<>(Game.NUMBER_OF_NUMBERS);
+        setRandomNumbers(random);
+    }
+
+    private void setRandomNumbers(Random random) {
+
+        for (int i=0; i<numbers.size(); i++) {
+            setNumber(i, random.nextInt(9) + 1);
+        }
+
+    }
+
+    private void setNumber(int index, int number) {
+        numbers.set(index, number);
+    }
+
+    private void clearNumbers() {
+        numbers.clear();
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+
 }
