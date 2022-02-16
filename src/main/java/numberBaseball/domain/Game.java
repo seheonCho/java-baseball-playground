146c849903fc648a4bab027ee45e53ab269a23e2
seheonCho <whtpgjs94@gmail.com>
2022-02-16 21:53:26 +0900

    feat: 게임시작

--- src/main/java/numberBaseball/domain/Game.java
+++ src/main/java/numberBaseball/domain/Game.java
@@ -1,4 +1,14 @@
 package numberBaseball.domain;
 
 public class Game {
+
+    final static int NUMBERS_OF_GAME = 3;
+
+    public void play() {
+        NumberCreater numberCreater = new NumberCreater();
+        Computer computer = new Computer(numberCreater.getNumbers());
+
+        Player player = new Player();
+    }
+
 }
